922791e58343676250d38a4e137671a139f9db2a
Aryo <chiefdevica@gmail.com>
2023-03-28 15:15:56 +0700

    first commit

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('mahasiswa.layout')
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+            Edit Mahasiswa
+            </div>
+            <div class="card-body">
+                @if ($errors->any())
+                <div class="alert alert-danger"><strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <form method="post" action="{{ route('mahasiswa.update', $Mahasiswa->nim) }}" id="myForm"> 
+                    @csrf 
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="nim">Nim</label>
+                        <input type="text" name="nim" class="formcontrol" id="nim" aria-describedby="nim" value="{{ $Mahasiswa->nim }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="nama" name="nama" class="formcontrol" id="nama" aria-describedby="nama" value="{{ $Mahasiswa->nama }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="kelas">Kelas</label>
+                        <input type="kelas" name="kelas" class="formcontrol" id="kelas" aria-describedby="password" value="{{ $Mahasiswa->kelas }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan</label>
+                        <input type="jurusan" name="jurusan" class="formcontrol" id="jurusan" aria-describedby="jurusan" value="{{ $Mahasiswa->jurusan }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="no_hp">No_Handphone</label>
+                        <input type="no_hp" name="no_hp" class="formcontrol" id="no_hp" aria-describedby="no_hp" value="{{ $Mahasiswa->no_hp }}">
+                    </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection 
